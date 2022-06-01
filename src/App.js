96022a7c38477570a8d3ef8429d8ff323327738d
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-06-01 15:33:03 +0200

    changed title for testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          welcome to Kuli
-        </a>
+        ></a> */}
+
+        <h1> welcome to Kuli</h1>
       </header>
     </div>
   );
