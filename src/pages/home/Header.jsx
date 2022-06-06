ab93690f109e81e3ad1bcff337670b8b06a8d9cd
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-06-06 21:34:41 +0200

    color variables, temporary styling

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -8,18 +8,27 @@ import { Button } from "../../components/button/Button";
 const Header = () => {
   return (
     <div className="header-container">
-      <img src={logo} className="logo" alt="logo"></img>
-      <h1>
-        Kuli is a place for finding opportunities and sharing insights about
-        local companies committed to gender equality.
-      </h1>
-      <div className="header-buttons-container">
-        <Link to={"/badges"}>
-          <Button title="to form" />
-        </Link>
-        <Link to={"/companies"}>
-          <Button title="to company list" />
-        </Link>
+      <div className="header-content">
+        <img src={logo} className="logo" alt="logo"></img>
+        <h1>
+          Kuli is a place for finding opportunities and sharing insights about
+          local companies committed to gender equality.
+        </h1>
+
+        <p>
+          Our mission is to support diverse talent and provide opportunities to
+          expand their network, and help local companies build diverse teams
+          where everyone can feel engaged and included.
+        </p>
+
+        <div className="header-buttons-container">
+          <Link to={"/badges"}>
+            <Button title="to form" color="dark-pink"/>
+          </Link>
+          <Link to={"/companies"}>
+            <Button title="to company list" color="dark-pink"/>
+          </Link>
+        </div>
       </div>
     </div>
   );
