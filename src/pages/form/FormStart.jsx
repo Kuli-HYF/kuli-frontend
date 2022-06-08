ae4ab3bc46c98e2c649d21c8d77d505ba1cbb50e
denrique-alvarez <denrique.alvarez@gmail.com>
2022-06-08 14:42:46 +0200

    quick fixes

--- src/pages/form/FormStart.jsx
+++ src/pages/form/FormStart.jsx
@@ -13,30 +13,35 @@ const FormStart = () => {
         </div>
         <div className="survey-intro__body-container">
           <p className="survey-intro__body-text">
-            The Kuli system was created to help employees and companies evaluate
-            gender equality inside the organization. In order to do this, Kuli
-            provides a series of questionnaires that allow users asses different
+            Kuli provides a questionnaire that allows users to asses different
             aspects of a company's working environment, internal dynamics and
             more.
           </p>
           <p className="survey-intro__body-text">
-            Questionnaires are divided into 5 different categories that focus on
+            Questionnaires are divided into 4 different categories that focus on
             different aspects:
           </p>
           <ul className="survey-intro__category-list">
-            <li className="survey-intro__category-list__item">Employment and compensation</li>
-            <li className="survey-intro__category-list__item">Work life balance and career development</li>
-            <li className="survey-intro__category-list__item">Health, safety and freedom from violence</li>
-            <li className="survey-intro__category-list__item">Governance and leadership</li>
+            <li className="survey-intro__category-list__item">
+              Employment and compensation
+            </li>
+            <li className="survey-intro__category-list__item">
+              Work life balance and career development
+            </li>
+            <li className="survey-intro__category-list__item">
+              Health, safety and freedom from violence
+            </li>
+            <li className="survey-intro__category-list__item">
+              Governance and leadership
+            </li>
           </ul>
-          <p className="survey-intro__body-text">Ready to join us?</p>
         </div>
         <div className="buttons">
-          <Link to={"/"}>
-            <Button title="Sign Up" color="dark-blue" />
-          </Link>
-          <Link to={"/"}>
+          <Link to={"/form"}>
             <Button title="To questionnaire" color="dark-blue" />
+          </Link>
+          <Link to={"/sign-up"}>
+            <Button title="Sign Up" color="dark-blue" />
           </Link>
         </div>
       </div>
