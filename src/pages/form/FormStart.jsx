1475b3086bf8869e994f2d528f63790eaee17e18
denrique-alvarez <denrique.alvarez@gmail.com>
2022-06-09 12:48:04 +0200

    survey intro with fade in, buttons, temporary badges and new bgcolor

--- src/pages/form/FormStart.jsx
+++ src/pages/form/FormStart.jsx
@@ -7,36 +7,49 @@ import { Button } from "../../components/button/Button";
 const FormStart = () => {
   return (
     <div className="container">
-      <div className="survey-intro__container">
+      <div className="survey-intro__container fadeIn">
         <div className="survey-intro__title">
-          <h2>Our System</h2>
+          <h2>Kuli Questionnaires</h2>
         </div>
         <div className="survey-intro__body-container">
           <p className="survey-intro__body-text">
             Kuli provides a questionnaire that allows users to asses different
             aspects of a company's working environment, internal dynamics and
-            more.
+            more. Questionnaires are divided into 4 different categories that
+            focus on different aspects:
           </p>
-          <p className="survey-intro__body-text">
-            Questionnaires are divided into 4 different categories that focus on
-            different aspects:
-          </p>
-          <ul className="survey-intro__category-list">
-            <li className="survey-intro__category-list__item">
-              Employment and compensation
-            </li>
-            <li className="survey-intro__category-list__item">
-              Work life balance and career development
-            </li>
-            <li className="survey-intro__category-list__item">
-              Health, safety and freedom from violence
-            </li>
-            <li className="survey-intro__category-list__item">
-              Governance and leadership
-            </li>
-          </ul>
+          <div className="category-list__container fadeIn">
+            <div className="survey-intro__category-list__item fadeIn">
+              <img
+                src="https://www.freepngimg.com/thumb/facebook/66701-golden-picture-badge-template-icon-free-download-png-hq.png"
+                alt="badge"
+              />
+              <p>Employment and compensation</p>
+            </div>
+            <div className="survey-intro__category-list__item fadeIn">
+              <img
+                src="https://www.freepngimg.com/thumb/facebook/66701-golden-picture-badge-template-icon-free-download-png-hq.png"
+                alt="badge"
+              />
+              <p>Work life balance and career development</p>
+            </div>
+            <div className="survey-intro__category-list__item fadeIn">
+              <img
+                src="https://www.freepngimg.com/thumb/facebook/66701-golden-picture-badge-template-icon-free-download-png-hq.png"
+                alt="badge"
+              />
+              <p>Health, safety and freedom from violence</p>
+            </div>
+            <div className="survey-intro__category-list__item fadeIn">
+              <img
+                src="https://www.freepngimg.com/thumb/facebook/66701-golden-picture-badge-template-icon-free-download-png-hq.png"
+                alt="badge"
+              />
+              <p>Governance and leadership</p>
+            </div>
+          </div>
         </div>
-        <div className="buttons">
+        <div className="buttons fadeIn">
           <Link to={"/form"}>
             <Button title="To questionnaire" color="dark-blue" />
           </Link>
