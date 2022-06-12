2581fabceff88a7839c85330133e10e547b1a6f0
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-06-12 16:57:57 +0200

    starting company search, fetching companies

--- src/pages/companies/CompanyStart.jsx
+++ src/pages/companies/CompanyStart.jsx
@@ -1,20 +1,51 @@
 import "./company.css";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { get } from "../../api/get";
 
 import Navigation from "../../components/navigation/Navigation";
 
 import { Button } from "../../components/button/Button";
 
 const CompanyStart = () => {
+  const [companies, setCompanies] = useState([]);
+
+  const fetchCompanies = async () => {
+    const result = await get("companies");
+    setCompanies(result.data);
+  };
+
+  useEffect(() => {
+    fetchCompanies();
+    console.log(companies);
+  }, []);
+
+  // const handleGet = async (event) => {
+  //   const search = event.target.parentElement.children[0].value;
+  //   const companiesUrl = "companies";
+  //   const result = await get(search);
+  //   console.log("get", result);
+  // };
+
   return (
     <>
       <Navigation />
-      <div className="temp-div">
-        <h2>companies home</h2>
+      <div className="companies-container">
+        <div className="intro-container">
+          <h2 className="companies-title">Search for a company</h2>
+          <p className="companies-intro-paragraph">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+            blanditiis magni sequi pariatur natus culpa neque reiciendis soluta,
+            ipsam aperiam quas ad vel ducimus debitis quos rerum, consequuntur
+            eum facilis.
+          </p>
 
-        <Link to={"/"}>
-          <Button title="home" color="temp-button dark-pink" />
-        </Link>
+          {/* <Link to={"/"}>
+            <Button title="home" color="temp-button dark-pink" />
+          </Link> */}
+          <input type="input"></input>
+          {/* <Button color="blue" action={handleGet} title="get" /> */}
+        </div>
       </div>
     </>
   );
