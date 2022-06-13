04b9cd781e2b2ff99d933e80ea0c16a162f8dc79
hansmbua <hansmbua@yahoo.com>
2022-06-13 15:05:05 +0200

    new

--- src/components/Signupcomponent/Signupcomponent.js
+++ src/components/Signupcomponent/Signupcomponent.js
@@ -1,15 +1,35 @@
 import {TextField} from "../Textfield/TextField";
 import './style.css';
-import post from '../../api/post';
 import {Formik , Form} from "formik";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as Yup from 'yup';
+import { ORIGIN } from "../../config";
 
 import React from 'react'
 
  export const Signup = () => {
 
-
+     const post = async (path, value) => {
+        const category = path;
+        const body = value;
+        const url = encodeURI(`${ORIGIN}${category}`);
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        const result = await response.json();
+        // console.log("add", result);
+        return result;
+      };
+      
+     
+      
 
 
 
@@ -54,9 +74,12 @@ import React from 'react'
     //here we make the post request 
 
     validationSchema = {validate} 
-    onSubmit = {values =>{
+    onSubmit = {data =>{
        //call the POST funtion here 
-        console.log(values)
+
+
+        console.log(data);
+        Signup.post(data);
 
         // const data = new FormData();
         // for (let i = 0; i < values.length; i++) {
@@ -71,13 +94,7 @@ import React from 'react'
       
 
         // })
-        const data = new FormData();
-        ajax({
-            type: 'POST',
-            url: 'https://kuli-strapi.herokuapp.com/api/',
-            data: data
-
-        });
+    
 
 
 
