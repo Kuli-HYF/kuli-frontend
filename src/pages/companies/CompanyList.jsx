35f6fe3cb020d1bfa36facecf1b3d2393f38fdd0
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-06-24 03:13:01 +0200

    improvements on companies search

--- src/pages/companies/CompanyList.jsx
+++ src/pages/companies/CompanyList.jsx
@@ -3,65 +3,52 @@ import { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import CompanyListItem from "./CompanyListItem";
 
-const CompanyList = ({ companies, selectedCompanies, resultsFound }) => {
+const CompanyList = ({
+  companies,
+  selectedCompanies,
+  handleOrder
+
+}) => {
   const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
-
   const [itemOffset, setItemOffset] = useState(0);
-  const itemsPerPage = 12;
+  const itemsPerPage = 8;
 
   useEffect(() => {
     let endOffset = itemOffset + itemsPerPage;
-    if (resultsFound) {
-      setCurrentItems(selectedCompanies.slice(itemOffset, endOffset));
-      setPageCount(Math.ceil(selectedCompanies.length / itemsPerPage));
-    } else {
-      setCurrentItems(companies.slice(itemOffset, endOffset));
-      setPageCount(Math.ceil(companies.length / itemsPerPage));
+    setCurrentItems(selectedCompanies.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(selectedCompanies.length / itemsPerPage));
+    if (selectedCompanies.length < 8) {
+      setItemOffset(0);
     }
-  }, [itemOffset, itemsPerPage, companies, selectedCompanies, resultsFound]);
+  }, [itemOffset, itemsPerPage, selectedCompanies]);
 
   const handlePageClick = (event) => {
-    if (resultsFound) {
-      let newOffset =
-        (event.selected * itemsPerPage) % selectedCompanies.length;
-      setItemOffset(newOffset);
-    } else {
-      let newOffset = (event.selected * itemsPerPage) % companies.length;
-      setItemOffset(newOffset);
-    }
+    let newOffset = (event.selected * itemsPerPage) % selectedCompanies.length;
+
+    setItemOffset(newOffset);
+
+    console.log(pageCount);
   };
 
   return (
     <>
       <div className="company-list-container">
-        {resultsFound ? (
-          <>
-            <div className="companies-found">
-              <p>{`Total companies found: ${selectedCompanies.length}`}</p>
+        <>
+          <div className="company-list-topbar">
+            <p>{`Total companies found: ${selectedCompanies.length}`}</p>
+            <button className="company-order-button" onClick={handleOrder}>
+              A ⇄ Z
+            </button>
+          </div>
+          {currentItems.map((company) => (
+            <div key={company.id} className="company-list-item">
+              <Link to={`/companies/${company.id}`} state={{ companies }}>
+                <CompanyListItem company={company} />
+              </Link>
             </div>
-            {currentItems.map((company) => (
-              <div key={company.id} className="company-list-item">
-                <Link to={`/companies/${company.id}`} state={{ companies }}>
-                  <CompanyListItem company={company} />
-                </Link>
-              </div>
-            ))}
-          </>
-        ) : (
-          <>
-            <div className="companies-found">
-              <p>{`Total companies found: ${companies.length}`}</p>
-            </div>
-            {currentItems.map((company) => (
-              <div key={company.id} className="company-list-item">
-                <Link to={`/companies/${company.id}`} state={{ companies }}>
-                  <CompanyListItem company={company} />
-                </Link>
-              </div>
-            ))}
-          </>
-        )}
+          ))}
+        </>
         <div className="company-paginate-wrapper">
           <ReactPaginate
             breakLabel="..."
