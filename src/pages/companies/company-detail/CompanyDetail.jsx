42112f4d1c03a3c43df170fd3a62dd0993a7527d
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-06-25 00:01:10 +0200

    company details css

--- src/pages/companies/company-detail/CompanyDetail.jsx
+++ src/pages/companies/company-detail/CompanyDetail.jsx
@@ -38,7 +38,7 @@ const CompanyDetail = () => {
     fetchDataBadge();
   }, [fetchDataCompany, fetchDataBadge]);
 
-  // console.log(company);
+  console.log(company);
   // console.log("badge: ", badge);
 
   return (
@@ -52,32 +52,41 @@ const CompanyDetail = () => {
                 {company ? company.name : "Loading"}
               </h1>
             </div>
+
+            <hr className="company-detail-line"></hr>
+
+            <div>
+              <p className="company-detail-address">
+                {company ? company.location.address : "Loading"}
+              </p>
+            </div>
             <div>
               <a href={company ? company.webpage : "#"}>
-                <p>
-                  {company
-                    ? company.webpage
-                    : "Loading"}
-                </p>
+                <p>{company ? company.webpage : "Loading"}</p>
               </a>
+            </div>
+
+            <div className="company-sector__container">
+              <h3 className="company-detail-sectors-title">Sectors:</h3>
+              <div className="company-sector-list-container">
+                {company ? (
+                  company.sectors.data.map((el, i) => (
+                    <div key={i} className="company-sector__list">
+                      <p>{el.attributes.name}</p>
+                    </div>
+                  ))
+                ) : (
+                  <p>Try again</p>
+                )}
+              </div>
             </div>
           </div>
 
           <div className="company-info__container">
-            <div className="company-sector__container">
-              <h3>Sectors:</h3>
-              {company ? (
-                company.sectors.data.map((el, i) => (
-                  <div key={i} className="company-sector__list">
-                    <p>{el.attributes.name}</p>
-                  </div>
-                ))
-              ) : (
-                <p>Try again</p>
-              )}
-            </div>
             <div className="company-badges">
-              <h2>Badges:</h2>
+              <h3 className="company-detail-badges-title">Badges:</h3>
+              <hr className="company-detail-badges-line"></hr>
+
               <div className="company-badges__container">
                 {badge && badge.length > 0 ? (
                   badge.map((el, i) => <CompanyBadge badge={el} key={i} />)
