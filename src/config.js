cd038dbdd3e9345ed952a80b5d95ff8e81118ac2
Aviv <89319835+aviv82@users.noreply.github.com>
2022-11-27 14:13:22 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const ORIGIN = "https://kuli-strapi.herokuapp.com/api/";
+// export const ORIGIN = "https://kuli-strapi.herokuapp.com/api/";
+export const ORIGIN = "https://kuli-backend-production.up.railway.app/api/";
+
